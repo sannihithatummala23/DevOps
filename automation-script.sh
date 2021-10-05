15f4f4ad38ec02783d639647a0d9cbfb3d9cfdd0
sannihithatummala23 <65324839+sannihithatummala23@users.noreply.github.com>
2021-10-05 13:11:56 -0600

    Update automation-script.sh

--- automation-script.sh
+++ automation-script.sh
@@ -31,6 +31,9 @@ kubectl port-forward service/romannumeralconverter-svc -n monitoring 8080:8080 &
 #Pull prometheus by executing the docker command:
 docker pull prom/prometheus
 
+echo -n "Update the <path/to/file>/project/DevOps/prometheus.yml > 'spring-actuator' targets:[xx:xx:xx:xx:8080] IP address configuration with the below k8 Service 'romannumeralconverter-svc' created Cluster-IP address:"
+kubectl describe svc -n monitoring | grep IPs | cut -d ":" -f2
+
 #Deploy prometheus by executing the docker command:
 #docker run -d --name prometheus -p 9090:9090 -v <path/to/file>/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
 
