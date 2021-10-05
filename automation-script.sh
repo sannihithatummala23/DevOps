fc42d502178b849917c4770e1e13b6e87364b8d7
sannihithatummala23 <65324839+sannihithatummala23@users.noreply.github.com>
2021-10-05 13:31:18 -0600

    Update automation-script.sh

--- automation-script.sh
+++ automation-script.sh
@@ -15,27 +15,45 @@ sleep 10
 # Change directory to DevOps dir
 cd DevOps
 
+echo " "
+
 # Create a nameSpace for k8 monitoring stack:
 kubectl create namespace monitoring
 
+echo " "
+
 #Execute the k8 deployment and service files in the namespace created:
 kubectl create -f deployment.yml -n monitoring
+
+echo " "
+
 kubectl create -f service.yml -n monitoring
 
 # Sleep for 60 seconds, just in case for the k8 resources to be up & running stable:
 sleep 60
 
+echo " "
+
 # Port-forwarding Cluster-ip service to localhost:8080 as a background process
 kubectl port-forward service/romannumeralconverter-svc -n monitoring 8080:8080 &
+
+echo " "
 
 #Pull prometheus by executing the docker command:
 docker pull prom/prometheus
 
-echo -n "Update the <path/to/file>/project/DevOps/prometheus.yml > 'spring-actuator' targets:[xx:xx:xx:xx:8080] IP address configuration with the below k8 Service 'romannumeralconverter-svc' created Cluster-IP address:"
+echo " "
+
+echo -n #################
+echo "*****Update the <path/to/file>/project/DevOps/prometheus.yml > 'spring-actuator' targets:[xx:xx:xx:xx:8080] IP address configuration with the below k8 Service 'romannumeralconverter-svc' created Cluster-IP address:*****"
+echo " "
 kubectl describe svc romannumeralconverter-svc -n monitoring | grep IPs | cut -d ":" -f2
+echo -n ##################
 
 #Deploy prometheus by executing the docker command:
 #docker run -d --name prometheus -p 9090:9090 -v <path/to/file>/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+
+echo " "
 
 #Deploy Grafana by executing the docker command:
 docker run -d --name grafana -p 3000:3000 grafana/grafana
