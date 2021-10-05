150583eed583f161ebf68c711f5ebdfe76bf8551
sannihithatummala23 <65324839+sannihithatummala23@users.noreply.github.com>
2021-10-05 09:41:16 -0600

    Update automation-script.sh

--- automation-script.sh
+++ automation-script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Make a directory project and change to that directory:
-mkdir project
-cd project
+mkdir project ; cd project
 
 # Cloning DevOps Repository that has required K8 and Prometheus files:
 git clone https://github.com/sannihithatummala23/DevOps.git
@@ -28,9 +27,6 @@ kubectl port-forward service/romannumeralconverter-svc 8080:8080 &
 
 #Pull prometheus by executing the docker command:
 docker pull prom/prometheus
-
-# Sleep for 15 seconds
-sleep 15
 
 #Deploy prometheus by executing the docker command:
 #docker run -d --name prometheus -p 9090:9090 -v <path/to/file>/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
